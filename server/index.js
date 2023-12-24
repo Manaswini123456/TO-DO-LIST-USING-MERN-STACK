e5ae393397e38b1e0e0600d77661a1c86ba67f5e
MANASWINI VOOLAPALLI <91537487+Manaswini123456@users.noreply.github.com>
2023-12-24 23:22:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,6 @@ const app = express();
 
 
 app.use(cors(
-    {
-        origin:["http://to-do-list-using-mern-stack-pbotdn6vm-manaswini123456.vercel.app"],
-        methods:["POST","GET","PUT","DELETE"],
-        credentials:true
-    }
 ));
 
 app.use(bodyParser.json({ extended: true }));
