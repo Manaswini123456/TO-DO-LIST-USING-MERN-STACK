95e672b971349e592e76490a7ee25a92e7d64471
Manaswini123456 <janu2001dec@gmail.com>
2023-12-24 18:44:29 +0530

    SERVER_NEW

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:["https://to-do-list-using-mern-stack-60pwrusko-manaswini123456.vercel.app/"],
+        origin:["https://to-do-list-using-mern-stack-fxf37vg05-manaswini123456.vercel.app/"],
         methods:["POST","GET","PUT","DELETE"],
         credentials:true
     }
